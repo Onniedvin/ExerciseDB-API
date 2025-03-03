f8f6d8579384536217af293d6a5f2e0b5d1e0f95
Onni Roivas <onni@Onni-Mac.local>
2025-03-03 14:08:57 +0200

    update flask app to use new exercise fetch

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from fetch_exercises import get_exercises, search_exercises
 
 app = Flask(__name__)
 
+all_exercises = get_exercises()
+
 @app.route("/")
-def hello_world():
-    return "Flask App"
+def home():
+    return "Kokeile http://127.0.0.1:8000/search?q=biceps"
+
+@app.route("/exercises", methods=["GET"])
+def list_exercises():
+    #Return all exercises as JSON
+    if not all_exercises:
+        return jsonify({"error": "Failed to fetch exercises"}), 500
+    return jsonify(all_exercises)
+
+@app.route("/search", methods=["GET"])
+def search():
+    # Search exercises based on a keyword
+    query = request.args.get("q", "").strip().lower()
+    if not query:
+        return jsonify({"error": "Query parameter 'q' is required"}), 400
+
+    results = search_exercises(all_exercises, query)
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)
